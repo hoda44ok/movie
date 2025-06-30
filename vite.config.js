6e861b8fd22d6ff642f2b6018b485cd075f0d189
hoda44ok <hoda.osama44@gmail.com>
2025-06-30 23:13:57 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/movie/",
-// });
-
 export default defineConfig({
-  base: "/movie/",
   plugins: [react()],
+  base: "/movie/",
 });
 
