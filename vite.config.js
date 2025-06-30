c77bfc8613365fc85674727bdc788221b8a99ded
hoda44ok <hoda.osama44@gmail.com>
2025-06-30 23:05:37 +0200

    update navbar or whatever the change is

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/movie/",
+// });
+
 export default defineConfig({
+  base: "/movie/",
   plugins: [react()],
-  base: "/movie",
 });
+
